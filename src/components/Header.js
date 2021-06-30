618e2bc1265d3b7dbbf17e2e56e6987cdb8643a7
Khaing Khant Htun <khaingkhanthtun@gmail.com>
2021-06-30 22:20:15 +0630

    added github link header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,15 @@ const Header = () => {
     const headerStyles = {
         textAlign: 'center',
         color: 'gray',
-        marginBottom: '-2rem',
+        marginBottom: '-1.5rem',
         marginTop: '1rem'
+    }
+    const linkStyle = {
+        color: '#6CB4EE'
     }
     return (
         <div className="max-w-xs sm:max-w-sm mx-auto">
-            <h1 style={headerStyles}>Built with ReactJS </h1>
+            <h1 style={headerStyles}>Built with ReactJS <a style={linkStyle} href="https://github.com/kkhtun/PhotoGallery-by-Pixabay-API">Github/kkhtun</a></h1>
         </div>
     )
 }
